198eb38ff834f93ee15c3706d6b3e924260637be
Ivan <ivan241578@gmail.com>
2022-05-25 18:22:23 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int exp = 1;
+while (b > 0)
+{
+    exp = exp * a;
+    b = b - 1;
+}
+Console.WriteLine(exp);
