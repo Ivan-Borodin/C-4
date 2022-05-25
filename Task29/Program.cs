16500ec14575bcfd14380a587bca05c4345cff56
Ivan <ivan241578@gmail.com>
2022-05-25 18:22:58 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
